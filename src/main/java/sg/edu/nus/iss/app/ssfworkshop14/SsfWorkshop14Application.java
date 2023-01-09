8673db657793da960b4bf16c667b360f29513d40
Wee Seng <wee.bscl@gmail.com>
2023-01-09 23:09:44 +0800

    init commit

---
+++ src/main/java/sg/edu/nus/iss/app/ssfworkshop14/SsfWorkshop14Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.app.ssfworkshop14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfWorkshop14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfWorkshop14Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/app/ssfworkshop14/SsfWorkshop14ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.app.ssfworkshop14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfWorkshop14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
